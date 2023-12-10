26f6c86035ef0120222d2764f8ce9ab81af2b1cd
Maksim Safronov <dev.safronov.maxim@gmail.com>
2023-12-10 15:10:08 +0400

    Fix memory leakage on joining threads #2

--- src/main/java/ru/vsk/async/rest/service/aggregator/AggregatorService.java
+++ src/main/java/ru/vsk/async/rest/service/aggregator/AggregatorService.java
@@ -1,7 +1,44 @@
 package ru.vsk.async.rest.service.aggregator;
 
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
-public interface AggregatorService {
-    List<String> aggregateData();
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+@Slf4j
+public abstract class AggregatorService<T> {
+    public List<T> aggregateData() {
+        Queue<CompletableFuture<T>> integrations = new LinkedList<>();
+        List<T> result = new ArrayList<>();
+        fetchData(integrations, result);
+        integrations.forEach(CompletableFuture::join);
+        integrations.clear();
+        return result;
+    }
+
+    public abstract void fetchData(Queue<CompletableFuture<T>> integrations, List<T> result);
+
+    protected void asyncCallIntegration(Queue<CompletableFuture<T>> integrations, Supplier<T> integrationCall, Consumer<T> resultList, int timeout, Runnable timeoutCallback) {
+        CompletableFuture<T> integrationCallData = CompletableFuture.supplyAsync(integrationCall);
+        integrations.add(integrationCallData);
+        integrationCallData
+                .completeOnTimeout(null, timeout, TimeUnit.SECONDS)
+                .whenComplete((data, ex) -> {
+                    if (ex != null) {
+                        log.error("Exception occurred while async service call. Error: {}", ex.getMessage());
+                    }
+                    if (data != null) {
+                        resultList.accept(data);
+                    }
+                    else {
+                        timeoutCallback.run();
+                    }
+                });
+    }
 }
--- src/main/java/ru/vsk/async/rest/service/aggregator/AsyncAggregatorServiceImpl.java
+++ src/main/java/ru/vsk/async/rest/service/aggregator/AsyncAggregatorServiceImpl.java
@@ -5,57 +5,23 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.vsk.async.rest.service.ApiService;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class AsyncAggregatorServiceImpl implements AggregatorService {
+public class AsyncAggregatorServiceImpl extends AggregatorService<String> {
 
     private final ApiService firstService;
     private final ApiService secondService;
     private final ApiService thirdService;
 
-    @SuppressWarnings("rawtypes")
-    private final List<CompletableFuture> integrations = new LinkedList<>();
-
     @Override
-    public List<String> aggregateData() {
-      
-        List<String> result = new ArrayList<>();
-
-        asyncCallIntegration(firstService::getData, result::add, 3, () -> log.error("First service didn't respond in time"));
-        asyncCallIntegration(secondService::getData, result::add, 3, () -> log.error("Second service didn't respond in time"));
-        asyncCallIntegration(thirdService::getData, result::add, 6, () -> log.error("Third service didn't respond in time"));
-
-        integrations.forEach(CompletableFuture::join);
-        integrations.clear();
-
-        return result;
-    }
-
-    private <T> void asyncCallIntegration(Supplier<T> integrationCall, Consumer<T> resultList, int timeout, Runnable timeoutCallback) {
-        CompletableFuture<T> integrationCallData = CompletableFuture.supplyAsync(integrationCall);
-        integrations.add(integrationCallData);
-        integrationCallData
-                .completeOnTimeout(null, timeout, TimeUnit.SECONDS)
-                .whenComplete((data, ex) -> {
-                    if (ex != null) {
-                        log.error("Exception occurred while async service call. Error: {}", ex.getMessage());
-                    }
-                    if (data != null) {
-                        resultList.accept(data);
-                    }
-                    else {
-                        timeoutCallback.run();
-                    }
-                });
+    public void fetchData(Queue<CompletableFuture<String>> integrations, List<String> result) {
+        asyncCallIntegration(integrations, firstService::getData, result::add, 3, () -> log.error("First service didn't respond in time"));
+        asyncCallIntegration(integrations, secondService::getData, result::add, 3, () -> log.error("Second service didn't respond in time"));
+        asyncCallIntegration(integrations, thirdService::getData, result::add, 6, () -> log.error("Third service didn't respond in time"));
     }
 }
